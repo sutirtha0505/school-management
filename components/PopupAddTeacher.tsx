4956a4c0e5ddc55e72d2cc8a1e0330d10aef2584
sutirtha0505 <tirtharajkarmakarinlinux@gmail.com>
2025-02-09 03:31:02 +0530

    Ala bala dala

--- components/PopupAddTeacher.tsx
+++ components/PopupAddTeacher.tsx
@@ -1,8 +1,10 @@
 "use client"
+import { useDropzone } from "react-dropzone";
 import Image from "next/image";
 import { useState, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { X } from "lucide-react";
 import {
     Select,
     SelectTrigger,
@@ -63,7 +65,25 @@ export default function PopupAddTeacher() {
     const handleComplete = () => {
         console.log("Form submitted:", formData);
     };
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>();
+    const [image, setImage] = useState<string | null>(null);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const file = acceptedFiles[0];
+        if (file) {
+            const imageUrl: string = URL.createObjectURL(file);
+            setImage(imageUrl);
+        }
+    }, []);
+    const removeImage = () => {
+        setImage(null);
+    };
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        accept: { 'image/*': [] },
+        maxFiles: 1,
+    });
 
 
     return (
@@ -84,10 +104,31 @@ export default function PopupAddTeacher() {
 
             <div className="mb-8">
                 {currentStep === 1 && (
-                    <div className="space-y-4">
+                    <div className="space-y-4 flex flex-col justify-center items-center">
                         <h2 className="text-accent-foreground text-md text-center mb-6">General Information</h2>
-                        <Input placeholder="Name" value={formData.name} onChange={(e) => handleInputChange("name", e.target.value)} />
-                        <Select value={formData.gender} onValueChange={(value: string) => handleInputChange("gender", value)}>
+                        {image ? (
+                            <div className="relative">
+                                <div className="w-32 h-32 rounded-full overflow-hidden border-2 border-primary">
+                                    <Image src={image} alt="Preview" width={128} height={128} className="object-cover w-full h-full" />
+                                </div>
+                                <button
+                                    className="absolute z-10 top-0 right-0 bg-red-500 text-white rounded-full p-1"
+                                    onClick={removeImage}
+                                >
+                                    <X size={16} />
+                                </button>
+                            </div>
+                        ) : (
+                            <div
+                                {...getRootProps()}
+                                className="w-32 h-32 flex items-center justify-center rounded-full border-2 border-dashed border-primary cursor-pointer hover:border-primary-dark transition-all"
+                            >
+                                <input {...getInputProps()} />
+                                {isDragActive ? <p className="text-center">Drop image here...</p> : <Image src="/person.png" width={50} height={50} alt="image"/>}
+                            </div>
+                        )}
+                        <Input placeholder="Name" value={formData.name} onChange={(e) => handleInputChange("name", e.target.value)} required/>
+                        <Select value={formData.gender} onValueChange={(value: string) => handleInputChange("gender", value)} required>
                             <SelectTrigger>
                                 <SelectValue placeholder="Gender" />
                             </SelectTrigger>
@@ -97,7 +138,21 @@ export default function PopupAddTeacher() {
                                 <SelectItem value="other">Other</SelectItem>
                             </SelectContent>
                         </Select>
-                        <Input placeholder="Blood Group" value={formData.bloodGroup} onChange={(e) => handleInputChange("bloodGroup", e.target.value)} />
+                        <Select value={formData.bloodGroup} onValueChange={(value: string) => handleInputChange("bloodGroup", value)} required>
+                            <SelectTrigger>
+                                <SelectValue placeholder="BloodGroup" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="A+">A+</SelectItem>
+                                <SelectItem value="B+">B+</SelectItem>
+                                <SelectItem value="AB+">AB+</SelectItem>
+                                <SelectItem value="O+">O+</SelectItem>
+                                <SelectItem value="A-">A-</SelectItem>
+                                <SelectItem value="B-">B-</SelectItem>
+                                <SelectItem value="AB-">AB-</SelectItem>
+                                <SelectItem value="O-">O-</SelectItem>
+                            </SelectContent>
+                        </Select>
                         <Popover>
                             <PopoverTrigger asChild>
                                 <Button
@@ -149,7 +204,7 @@ export default function PopupAddTeacher() {
                 )}
             </div>
 
-            <Button className="w-full bg-primary hover:bg-green-500 text-black font-bold py-3 rounded-lg" onClick={currentStep === 3 ? handleComplete : handleNext}>
+            <Button className="w-full text-accent-foreground font-bold py-3 rounded-lg" onClick={currentStep === 3 ? handleComplete : handleNext}>
                 {currentStep === 3 ? "Complete" : "Next"}
             </Button>
         </>
